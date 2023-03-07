b30000517707e6b522b5e81804340c41d163a49b
Antoine Duval <antoine@cuffel.fr>
2023-03-07 23:08:16 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,74 @@
+const LIBRARIES = {
+    FS: require("fs"),
+    NodeCMD: require("node-cmd"),
+    Path: require("path"),
+    Express: require('express')
+};
+
+// We check that the OS is MacOS.
+if(process.platform !== "darwin"){
+    console.log("This program is only compatible with MacOS.")
+    return;
+}
+
+// We create the folder storing the temporary files if it does not exist.
+const MP3_DIRECTORY = LIBRARIES.Path.resolve(__dirname + "/mp3/");
+if (!LIBRARIES.FS.existsSync(MP3_DIRECTORY)){
+    LIBRARIES.FS.mkdirSync(MP3_DIRECTORY);
+}
+
+// Empty the MP3 folder if there are files left
+LIBRARIES.FS.readdir(MP3_DIRECTORY, (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+        LIBRARIES.FS.unlink(LIBRARIES.Path.join(MP3_DIRECTORY, file), (err) => {
+            if (err) throw err;
+        });
+    }
+});
+
+// Initialize the web server.
+const app = LIBRARIES.Express()
+app.get('/', function (req, res) {
+    res.redirect('/api');
+})
+app.get('/api', function (req, res) {
+    const ATTR = "sentence";
+    const SENTENCE = req.query[ATTR];
+    
+    if(SENTENCE){
+        const FILE_NAME = "/MacTTS-" + new Date().getTime();
+        const ABSOLUTE_PATH = LIBRARIES.Path.resolve(__dirname + "/mp3/") + FILE_NAME;
+        const COMMAND = "say -o \"" + ABSOLUTE_PATH + ".aiff\" \"" + SENTENCE + "\"";
+
+        LIBRARIES.NodeCMD.run(
+            COMMAND,
+            function(err, data, stderr){
+                // Convert AIFF file to MP3
+                const FFMPEG_ABSOLUTE_PATH = LIBRARIES.Path.resolve(__dirname + "/ffmpeg");
+                const CONVERT_COMMAND = FFMPEG_ABSOLUTE_PATH + " -i " + ABSOLUTE_PATH + ".aiff -f mp3 -acodec libmp3lame -ab 192000 -ar 44100 " + ABSOLUTE_PATH + ".mp3";
+                LIBRARIES.NodeCMD.run(
+                    CONVERT_COMMAND,
+                    function(err, data, stderr){
+                        // Delete the old AIFF file
+                        LIBRARIES.FS.unlink(ABSOLUTE_PATH + ".aiff", function(){
+                            const MP3_FILE_PATH = ABSOLUTE_PATH + ".mp3";
+                            res.sendFile(MP3_FILE_PATH);
+                            res.on('finish', function() {
+                                LIBRARIES.FS.unlink(MP3_FILE_PATH, (err) => {
+                                    if (err) throw err;
+                                });
+                            });
+                        });
+                    }
+                );
+            }
+        );
+    }
+    else{
+        res.send('You must enter your sentence in the "' + ATTR + '" parameter (Example: http://localhost/api?' + ATTR + '=Hello).')
+    }
+})
+
+app.listen(80)
